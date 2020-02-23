2c5332fc88ff1c7bcda6e59e45704532ccf23122
kings10-hiroki <h61147366@gmail.com>
2020-02-23 21:43:23 +0900

    complete

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -6,6 +6,8 @@
 
 require('./bootstrap');
 
+require('./parts/markdown');
+
 window.Vue = require('vue');
 
 /**
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,10 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Auth\PostController@showTopPage')->name('top');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/drafts/new', 'Auth\PostController@index')->name('drafts.new');
+Route::post('/drafts/new', 'Auth\PostController@postArticle')->name('drafts.new.posts');
+
+Route::get('/drafts/{id}', 'Auth\PostController@showArticle')->name('item');
